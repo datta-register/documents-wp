a95fbf74e3822170f95e0a7d87b6f1fd7dc80fb1
Gunjan Datta <me@dattabase.com>
2021-10-07 10:04:17 -0400

    Added spfx solution.

---
+++ spfx/src/webparts/documentsView/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDocumentsViewWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DocumentsViewWebPartStrings' {
+  const strings: IDocumentsViewWebPartStrings;
+  export = strings;
+}
