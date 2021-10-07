c8f3e0e5f3d7fe367d78d569c10dedbb526b9b05
Gunjan Datta <me@dattabase.com>
2021-10-07 10:10:11 -0400

    Removed unused code.

--- src/ds.ts
+++ src/ds.ts
@@ -11,24 +11,6 @@ export interface IItem extends Types.SP.FileOData {
  * Data Source
  */
 export class DataSource {
-    // Gets the item id from the query string
-    static getItemIdFromQS() {
-        // Get the id from the querystring
-        let qs = document.location.search.split('?');
-        qs = qs.length > 1 ? qs[1].split('&') : [];
-        for (let i = 0; i < qs.length; i++) {
-            let qsItem = qs[i].split('=');
-            let key = qsItem[0];
-            let value = qsItem[1];
-
-            // See if this is the "id" key
-            if (key == "ID") {
-                // Return the item
-                return parseInt(value);
-            }
-        }
-    }
-
     // Initializes the application
     static init(): PromiseLike<void> {
         // Return a promise
